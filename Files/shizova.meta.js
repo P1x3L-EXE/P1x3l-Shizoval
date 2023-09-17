2f1f676d76c8d63abc041a914dce3783dc27995b
P1x3L-EXE <138071807+P1x3L-EXE@users.noreply.github.com>
2023-09-17 14:08:47 +0100

    Add files via upload

---
+++ Files/shizova.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name User
+// @version 1.0
+// @description 
+// @author User
+// @match https://*.tankionline.com/*
+// @require https://raw.githubusercontent.com/flyover/imgui-js/master/dist/imgui.umd.js
+// @require https://raw.githubusercontent.com/flyover/imgui-js/master/dist/imgui_impl.umd.js
+// @icon https://github.com/P1x3L-EXE/P1x3l-Shizoval/raw/main/heart.png
+// @run-at document-start
+// @grant GM_xmlhttpRequest
+// @grant unsafeWindow
+// ==/UserScript==
